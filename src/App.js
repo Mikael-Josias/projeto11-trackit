27830f6a6421c9e85464d1c2a2d31c400486ea18
Mikael Josias <mikaeljosias2002@gmail.com>
2022-12-14 12:07:52 -0300

    style: added global style and default font

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import LoginPage from "./pages/LoginPage";
+import GlobalStyle from "./styles/GlobalStyle";
+import "./styles/fonts.css";
+
+function App() {
+	return (
+		<>
+			<GlobalStyle/>
+			<LoginPage/>
+		</>
+	);
+}
+
+export default App;

d3426525668b456f2f1866dfea98005d264fbb1a
Mikael Josias <mikaeljosias2002@gmail.com>
2022-12-14 13:09:07 -0300

    feat: redirect to login page when authentication token dos not exist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import LoginPage from "./pages/LoginPage";
-import GlobalStyle from "./styles/GlobalStyle";
-import "./styles/fonts.css";
+import { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 function App() {
+	const navigate = useNavigate();
+	const [authToken, setAuthToken] = useState(null);
+
+	useEffect(() => {
+		if (authToken === null) {
+			navigate("/");
+		}
+		
+	}, []);
+
 	return (
-		<>
-			<GlobalStyle/>
-			<LoginPage/>
-		</>
+		<Routes>
+			<Route path="/" element={<LoginPage/>} />
+		</Routes>
 	);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import "./styles/fonts.css";
+import GlobalStyle from './styles/GlobalStyle';
 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<GlobalStyle/>
+		
+		<BrowserRouter>
+			<App/>
+		</BrowserRouter>
+	</React.StrictMode>
 );

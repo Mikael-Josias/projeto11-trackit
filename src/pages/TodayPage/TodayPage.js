9264611461c3926fa77ab741c31ed8bafd3d554f
Mikael Josias <mikaeljosias2002@gmail.com>
2022-12-15 11:23:20 -0300

    feat: redirected to log in page when user data is lost

--- src/pages/TodayPage/TodayPage.js
+++ src/pages/TodayPage/TodayPage.js
@@ -1,7 +1,25 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { UserContext } from "../../contexts/UserContext";
+
+import TopHeader from "../../components/TopHeader/TopHeader";
+
 
 export default function TodayPage(){
+    const navigate = useNavigate();
+    const { userData } = useContext(UserContext);
+    
+    useEffect(() => {
+        if (userData === undefined) {
+            alert("Desculpe, sua sessão expirou!");
+            navigate("/");
+        }
+    }, [userData]);
+
     return (
         <>
+            <TopHeader/>
             Today is the day!
         </>
     );

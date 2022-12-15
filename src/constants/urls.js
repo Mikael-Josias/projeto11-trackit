a6f188f57057cab8cf97e8450df0a184273d6a35
Mikael Josias <mikaeljosias2002@gmail.com>
2022-12-14 23:17:21 -0300

    feat: verify user identity with api

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,2 +1,3 @@
 
 export const registerNewUserUrl = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up";
+export const logInUserUrl = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login";
--- src/pages/SignInPage/SignInPage.js
+++ src/pages/SignInPage/SignInPage.js
@@ -1,20 +1,44 @@
-import { SignSpan } from "./styled";
+import { useState } from "react";
+import axios from "axios";
+
 import logo from "../../assets/logo.svg";
+
 import SignFields from "../../components/SignFields/SignFields";
-import { Link } from "react-router-dom";
 import Input from "../../components/Input/Input";
 import LogoImage from "../../components/LogoImage/LogoImage";
 import SignContainer from "../../components/SignContainer/SignContainer";
 import SignLink from "../../components/SignLink/SignLink";
-
+import { logInUserUrl } from "../../constants/urls";
 
 export default function SignInPage(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function LogIn(e){
+        e.preventDefault();
+        
+        const user = {
+            email,
+            password
+        }
+
+        const promisse = axios.post(logInUserUrl, user);
+        promisse.then((res) => {
+            console.log(res.data);
+        });
+        promisse.catch((err) => {
+            console.log(err.message);
+        });
+    }
+
     return (
         <SignContainer>
             <LogoImage src={logo} />
-            <SignFields>
-                <Input type="email" placeholder="email" required />
-                <Input type="password" placeholder="senha" required />
+
+            <SignFields onSubmit={(e) => LogIn(e)} >
+                <Input type="email" placeholder="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+                <Input type="password" placeholder="senha" required value={password} onChange={(e) => setPassword(e.target.value)} />
+
                 <Input type="submit" value="Entrar"/>
             </SignFields>
 
